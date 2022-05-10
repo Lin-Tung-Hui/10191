fa68e5aa665d1d158a91c224ea90756d6a1ddbb4
Alex <alexlin@weintek.com>
2022-05-10 09:26:14 +0800

    First commit

--- main.cpp
+++ main.cpp
@@ -1,120 +1,103 @@
-#include <iostream>
 #include <string>
+#include <stack>
+#include <deque>
+#include <iostream>
 #include <sstream>
-#include <vector>
-#include <algorithm>
 
-#ifdef GTEST
-#include <gtest/gtest.h>
-#endif
+//#define GTEST
 
-struct appointment {
-    int start_time;
-    int total_time;
+class Rail
+{
+public:
+    Rail(std::stringstream &permutation, int number)
+        : number_(number)
+    {
+        init_rail_A();
+        init_rail_B(permutation);
+    };
+    bool check_permutation();
+
+public:
+    void init_rail_A();
+    void init_rail_B(std::stringstream &permutation);
+
+private:
+    int number_;
+    std::deque< int > rail_A;
+    std::deque< int > rail_B;
+    std::stack< int > station;
 };
 
-struct LongestSnap {
-    int day;
-    int start_time;
-    int total_time;
-};
-
-struct LongestSnap count_longest_snap(std::vector< appointment > &appointments)
+void Rail::init_rail_A()
 {
-    int longest_snap_time = 0;
-    int longest_snap_begin = 0;
-    int total_nums = appointments.size();
-    int snap_begin = 600, snap_end = 0, tmp_snap_time;
-    int i = 0;
-    while (snap_end < 1080) {
-        while (snap_begin >= appointments[i].start_time && (i < total_nums)) {
-            if (snap_begin < appointments[i].start_time + appointments[i].total_time)
-                snap_begin = appointments[i].start_time + appointments[i].total_time;
-            i++;
-        }
-
-        if (i == appointments.size())
-            snap_end = 1080;
-        else
-            snap_end = appointments[i].start_time;
-
-        tmp_snap_time = snap_end - snap_begin;
-        if (tmp_snap_time > longest_snap_time) {
-            longest_snap_time = tmp_snap_time;
-            longest_snap_begin = snap_begin;
-        }
-        snap_begin = snap_end;
+    for (int i = 1; i <= number_; i++)
+        rail_A.push_back(i);
+}
+void Rail::init_rail_B(std::stringstream &permutation)
+{
+    for (int i = 0; i < number_; i++) {
+        int number;
+        permutation >> number;
+        rail_B.push_back(number);
     }
-
-    LongestSnap longest_snap;
-    longest_snap.start_time = longest_snap_begin;
-    longest_snap.total_time = longest_snap_time;
-
-    return longest_snap;
 }
 
-struct LongestSnap get_longest_snap(std::istream &is, int number, int day)
+bool Rail::check_permutation()
 {
-    std::string input;
-    LongestSnap longest_snap;
-    std::vector< appointment > appointments;
+    while (!rail_B.empty()) {
+        if (rail_B.front() >= rail_A.front()) {
+            station.push(rail_A.front());
+            rail_A.pop_front();
+        }
 
-    for (int i = 0; i < number; i++) {
-        std::getline(is, input);
+        while ((!rail_B.empty()) && (!station.empty())) {
 
-        std::stringstream ss(input);
-        struct appointment my_appointment;
-        int hh1, mm1, hh2, mm2;
-        char ch;
-        ss >> hh1 >> ch >> mm1;
-        ss >> hh2 >> ch >> mm2;
-        my_appointment.start_time = (hh1 * 60 + mm1);
-        my_appointment.total_time = (hh2 * 60 + mm2) - my_appointment.start_time;
+            if (!rail_A.empty()) {
+                if ((rail_A.front() > rail_B.front()) && (station.top() > rail_B.front()))
+                    return false;
+            }
 
-        appointments.push_back(my_appointment);
+            if (station.top() == rail_B.front()) {
+                rail_B.pop_front();
+                station.pop();
+            } else
+                break;
+        }
+
+        if (rail_A.empty() && (!rail_B.empty())) {
+            return false;
+        }
     }
-    std::sort(appointments.begin(), appointments.end(), [](appointment a, appointment b) {
-        return a.start_time < b.start_time;
-    });
-
-    longest_snap = count_longest_snap(appointments);
-    longest_snap.day = day;
-
-    return longest_snap;
-}
-
-bool dump_longest_snap(struct LongestSnap longest_snap, std::ostream &os)
-{
-    std::stringstream output;
-    output << "Day #" << longest_snap.day << ": the longest nap starts at ";
-    char start_time[6];
-    snprintf(start_time, 6, "%02d:%02d", (longest_snap.start_time / 60), (longest_snap.start_time % 60));
-    output << start_time;
-    output << " and will last for ";
-    if (longest_snap.total_time >= 60) {
-        output << (longest_snap.total_time / 60) << " hours and ";
-    }
-    output << (longest_snap.total_time % 60) << " minutes." << std::endl;
-    os << output.str();
 
     return true;
 }
 
-void uva10191(std::istream &is, std::ostream &os)
+void uva541(std::istream &is, std::ostream &os)
 {
-    int day = 1;
     while (true) {
-        std::string input;
-        std::getline(is, input);
-        std::stringstream ss;
-        ss << input;
+        std::string line;
+        std::getline(is, line);
 
-        int number = 0;
-        ss >> number;
-        if (number <= 0)
+        if (line == "")
             break;
-        dump_longest_snap(get_longest_snap(is, number, day), os);
-        day++;
+
+        int number = atoi(line.c_str());
+        if (number == 0)
+            break;
+
+        while (true) {
+            std::getline(is, line);
+            if (atoi(line.c_str()) == 0) {
+                os << std::endl;
+                break;
+            }
+            std::stringstream permutation(line);
+            Rail uva541_rail(permutation, number);
+            if (uva541_rail.check_permutation() == true)
+                os << "Yes\n";
+            else
+                os << "No\n";
+        }
     }
 }
 
@@ -125,6 +108,7 @@ int main(int argc, char **argv)
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 #else
-    uva10191(std::cin, std::cout);
+    uva541(std::cin, std::cout);
 #endif
+    return 0;
 }
--- unittest.cpp
+++ unittest.cpp
@@ -4,56 +4,128 @@
 #include <sstream>
 #include "main.cpp"
 
-TEST(longest_snap, test_1)
+TEST(rail, test_1)
 {
-    std::istringstream iss(R"--(4
-10:00 12:00 Lectures
-12:00 13:00 Lunch, like always.
-13:00 15:00 Boring lectures...
-15:30 17:45 Reading
+    std::istringstream iss(R"--(5
+1 2 3 4 5
+5 4 1 2 3
+1 4 3 2 5
+3 4 2 5 1
+3 4 2 1 5
+4 3 5 2 1
+0
+3
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+0
 4
-10:00 12:00 Lectures
-12:00 13:00 Lunch, just lunch.
-13:00 15:00 Lectures, lectures... oh, no!
-16:45 17:45 Reading (to be or not to be?)
-4
-10:00 12:00 Lectures, as everyday.
-12:00 13:00 Lunch, again!!!
-13:00 15:00 Lectures, more lectures!
-15:30 17:15 Reading (I love reading, but should I schedule it?)
-1
-12:00 13:00 I love lunch! Have you ever noticed it? :)
-1
-12:00 16:00 I love lunch! Have you ever noticed it? :)
-2
-10:00 16:00 Reading......
-16:01 18:00 Take exercise(table tennis with Professor Z)
+1 2 3 4
+1 2 4 3
+1 3 2 4
+1 3 4 2
+1 4 2 3
+1 4 3 2
+2 1 3 4
+2 1 4 3
+2 3 1 4
+2 3 4 1
+2 4 1 3
+2 4 3 1
+3 1 2 4
+3 1 4 2
+3 2 1 4
+3 2 4 1
+3 4 1 2
+3 4 2 1
+4 1 2 3
+4 1 3 2
+4 2 1 3
+4 2 3 1
+4 3 1 2
+4 3 2 1
+0
+10
+5 7 10 9 8 6 4 3 2 1
+5 6 4 8 7 3 2 9 1 10
+0
+0
 )--");
 
     std::ostringstream oss;
 
-    uva10191(iss, oss);
+    uva541(iss, oss);
 
-    EXPECT_EQ(
-            "Day #1: the longest nap starts at 15:00 and will last for 30 minutes.\n"
-            "Day #2: the longest nap starts at 15:00 and will last for 1 hours and 45 minutes.\n"
-            "Day #3: the longest nap starts at 17:15 and will last for 45 minutes.\n"
-            "Day #4: the longest nap starts at 13:00 and will last for 5 hours and 0 minutes.\n"
-            "Day #5: the longest nap starts at 10:00 and will last for 2 hours and 0 minutes.\n"
-            "Day #6: the longest nap starts at 16:00 and will last for 1 minutes.\n",
-            oss.str());
+    EXPECT_EQ(R"--(Yes
+No
+Yes
+Yes
+Yes
+Yes
+
+Yes
+Yes
+Yes
+Yes
+No
+Yes
+
+Yes
+Yes
+Yes
+Yes
+No
+Yes
+Yes
+Yes
+Yes
+Yes
+No
+Yes
+No
+No
+Yes
+Yes
+No
+Yes
+No
+No
+No
+No
+No
+Yes
+
+Yes
+Yes
+
+)--",
+              oss.str());
 }
-TEST(longest_snap, test_2)
+
+TEST(rail, test_2)
 {
-    std::istringstream iss("4\n"
-                           "10:00 12:00 Lectures\n"
-                           "12:00 13:00 Lunch, like always.\n"
-                           "12:00 12:05 Boring lectures...\n"
-                           "15:30 17:45 Reading\n");
+    std::istringstream iss(R"--(5
+1 2 3 4 5
+5 4 1 2 3
+0
+6
+6 5 4 3 2 1
+0
+0
+)--");
 
     std::ostringstream oss;
 
-    uva10191(iss, oss);
+    uva541(iss, oss);
 
-    EXPECT_EQ("Day #1: the longest nap starts at 13:00 and will last for 2 hours and 30 minutes.\n", oss.str());
+    EXPECT_EQ(
+            "Yes\n"
+            "No\n"
+            "\n"
+            "Yes\n"
+            "\n",
+            oss.str());
 }
